67079d32555b0c00f669a5a6f2ea72b689e238f3
Vegeta <56867717+pozengineer@users.noreply.github.com>
2020-02-21 23:39:11 +1100

    initial upload to gitHub, all functions are working

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burgerName) VALUES ('Meeses');
+INSERT INTO burgers (burgerName) VALUES ('Bobbi');
+INSERT INTO burgers (burgerName, devour) VALUES ('Sylvester', true);
+INSERT INTO burgers (burgerName, devour) VALUES ('Marilyn Manson', true);
+INSERT INTO burgers (burgerName, devour) VALUES ('Joe Biden', true);
+INSERT INTO burgers (burgerName) VALUES ('Owens');
---
+++ public/assets/javaScript/burgers.js
@@ -0,0 +1,106 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+    $(".change-devour").on("click", function (event) {
+        var id = $(this).data("id");
+        var newDevour = $(this).data("newdevour");
+
+        var newDevourState = {
+            devour: newDevour
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function () {
+                console.log("changed devour to", newDevour);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        var newBurger = {
+            burgerName: $("#ca").val().trim(),
+            devour: $("[name=devour]:checked").val().trim()
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".delBurger").on("click", function (event) {
+        console.log("Delete button clicked");
+        var id = $(this).data("id");
+        console.log(id);
+
+        // Send the DELETE request.
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function () {
+                console.log("deleted id ", id);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $("#addmovie").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        var newMovie = {
+            movie: $("#addmovie [name=movie]").val().trim()
+        };
+
+        // Send the POST request.
+        $.ajax("/api/movies", {
+            type: "POST",
+            data: newMovie
+        }).then(
+            function () {
+                console.log("added new movie");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $("#updatemovie").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        var id = $("[name=id]").val().trim();
+
+        var updatedMovie = {
+            movie: $("#updatemovie [name=movie]").val().trim()
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/movies/" + id, {
+            type: "PUT",
+            data: updatedMovie
+        }).then(
+            function () {
+                console.log("updated id ", id);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});  
